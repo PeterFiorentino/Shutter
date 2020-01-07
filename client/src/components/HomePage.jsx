27da9a12920c679790f1aeca6c8e9d4a965dd325
Bpmauge <briahanamauge@pursuit.org>
2020-01-07 00:09:52 -0500

    added picture display component and added the approiate props

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,34 +1,36 @@
 import React from "react"
 import axios from "axios"
+import PictureDisplay from "./PictureDisplay";
 
-class HomePage extends React.Component{
-    constructor(props){
+class HomePage extends React.Component {
+    constructor(props) {
         super()
         this.state = {
             username: props.userName,
-            email: '',
-            urls: [],
-            alt:[],
-            caption: []
+            pictures: []
         }
     }
     getAllPictures = async () => {
-        const {username} = this.state
+        const { username } = this.state
         console.log(username)
         let response = await axios.get(`http://localhost:3001/images/users/${username}`);
-        console.log(response)
+        console.log(response.data.body)
+        this.setState({
+            pictures: response.data.body
+        })
 
     }
-    render(){
+    render() {
         return (
             <div>
                 <h1>Home Page welcome {this.props.userName}</h1>
-                <button onClick = {this.getAllPictures}>Click me</button>
+                <button onClick={this.getAllPictures}>Click me</button>
+                <PictureDisplay pictures = {this.state.pictures}/>
             </div>
         )
 
     }
-    
+
 }
 
 export default HomePage
