f8ebdd5d98f96aa5d13182ee53530d79667f6b5c
Bpmauge <briahanamauge@pursuit.org>
2020-01-06 23:31:53 -0500

    added get all images function

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,14 +1,29 @@
 import React from "react"
+import axios from "axios"
 
-class  HomePage extends React.Component{
-    constructor(){
+class HomePage extends React.Component{
+    constructor(props){
         super()
+        this.state = {
+            username: props.userName,
+            email: '',
+            urls: [],
+            alt:[],
+            caption: []
+        }
     }
-    
+    getAllPictures = async () => {
+        const {username} = this.state
+        console.log(username)
+        let response = await axios.get(`http://localhost:3001/images/users/${username}`);
+        console.log(response)
+
+    }
     render(){
         return (
             <div>
                 <h1>Home Page welcome {this.props.userName}</h1>
+                <button onClick = {this.getAllPictures}>Click me</button>
             </div>
         )
 
