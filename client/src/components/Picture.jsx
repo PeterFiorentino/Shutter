5ea53c55bc43388b0af328c88f846eb3f51583ea
Bpmauge <briahanamauge@pursuit.org>
2020-01-06 20:23:29 -0500

    completed the picture component

--- client/src/components/Picture.jsx
+++ client/src/components/Picture.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+const Picture = (props) => {
+    return (
+        <>
+        <img
+        src = {props.url}
+        alt = {props.alt}
+        width = '300px'
+        height = '300px'
+        />
+        </>
+    )
+}
+export default Picture;
