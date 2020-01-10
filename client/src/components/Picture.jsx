e8b68f3c3a6bb6b592ff36ed3f6a61e6388755e8
Bpmauge <briahanamauge@pursuit.org>
2020-01-10 15:34:47 -0500

    removed the p tag with photo information

--- client/src/components/Picture.jsx
+++ client/src/components/Picture.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
+
 const Picture = (props) => {
+    console.log(props)
     return (
-        <div className = 'picture'>
-        
         <img 
         src = {props.url}
         alt = {props.alt}
         width = '300px'
         height = '300px'
         />
-         <p><strong>{props.username}</strong> {props.caption} <em>{props.hashtag}</em></p>
-        </div>
+   
     )
 }
 export default Picture;
