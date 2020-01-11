3f8b05c9c2ec4f34fde6c478d86981c836b3fa61
Bpmauge <briahanamauge@pursuit.org>
2020-01-11 16:52:02 -0500

    get the picture when you click on it

--- client/src/components/Picture.jsx
+++ client/src/components/Picture.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import Interactions from './Interactions';
+import axios from 'axios';
 
+class Picture extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            username: props.username,
+            poster_name: props.poster_name,
+            caption: props.caption,
+            hashtag: props.hashtag,
+            id: props.id,
+            url: props.url,
+            alt: props.alt
+        }
+    }
+    getSinglePicture = async () => {
+        const {id} = this.state;
+        try {
+            const res = await axios.get(`http://localhost:3001/images/${id}`)
+            console.log(res.data.payload[0])
 
-const Picture = (props) => {
-    return (
-        <>
-        <img 
-        src = {props.url}
-        alt = {props.alt}
-        width = '300px'
-        height = '300px'
-        />
-         <Interactions username = {props.username} poster_name={props.poster_name} caption={props.caption} hashtag={props.hashtag} id={props.id}/>
-   </>
-    )
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    render() {
+        const { username, poster_name, caption, hashtag, id, url, alt } = this.state
+        return (
+            <>
+                <img onClick={this.getSinglePicture}
+                    src={url}
+                    alt={alt}
+                    width='300px'
+                    height='300px'
+                />
+                <Interactions username={username} poster_name={poster_name} caption={caption} hashtag={hashtag} id={id} />
+            </>
+        )
+    }
 }
 export default Picture;
