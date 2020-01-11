635aa6155c9d0ced6979705ebfdc89e044087e38
Bpmauge <briahanamauge@pursuit.org>
2020-01-11 11:54:12 -0500

    refactored code

--- client/src/components/Picture.jsx
+++ client/src/components/Picture.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import Interactions from './Interactions';
 
 
 const Picture = (props) => {
     return (
+        <>
         <img 
         src = {props.url}
         alt = {props.alt}
         width = '300px'
         height = '300px'
         />
-   
+         <Interactions username = {props.username} poster_name={props.poster_name} caption={props.caption} hashtag={props.hashtag} id={props.id}/>
+   </>
     )
 }
 export default Picture;
